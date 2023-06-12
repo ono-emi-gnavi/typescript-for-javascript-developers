f524407392753c84a06b3a49dfc980b36023c87c
ono-emi <ono-emi@gnavi.co.jp>
2023-06-12 13:57:05 +0900

    create boolean.ts

---
+++ src/boolean.ts
@@ -0,0 +1,10 @@
+export {};
+
+let name = 'TypeScript';
+// すでにグローバル定数にあるので、再定義できない
+
+// let isFinished = true;
+let isFinished: boolean = true;
+isFinished = false;
+// isFinished = 1;
+console.log({ isFinished });
