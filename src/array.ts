804a42b323397225939a37b0c11fe9a1a72579be
ono-emi <ono-emi@gnavi.co.jp>
2023-06-14 14:24:14 +0900

    create array.ts

---
+++ src/array.ts
@@ -0,0 +1,19 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+// 非推奨
+let numbers2: Array<number> = [1, 2, 3];
+
+let strings2: Array<string> = ['Tokyo','Osaka','Kyoto'];
+
+let strings: string[] = ['TypeScript', 'JavaScript', 'CoffeeScript'];
+
+let nijigenhairetu: number[][] = [
+  [50,100],
+  [150,200]
+];
+
+let hairetu: (string | number | boolean)[] = [1,false,'Japan'];
+
+
