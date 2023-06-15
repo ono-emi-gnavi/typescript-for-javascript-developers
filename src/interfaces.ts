cb945a054441c1a45c6cf6c63b260082d85838af
ono-emi <ono-emi@gnavi.co.jp>
2023-06-15 11:37:29 +0900

    create interfaces.ts

---
+++ src/interfaces.ts
@@ -0,0 +1,15 @@
+export {};
+type ObjectType = { 
+  name:string; 
+  age:number;
+};
+
+interface ObjectInterfacce {
+  name:string; 
+  age:number;
+}
+
+let object: ObjectInterfacce = {
+  name: 'Ham',
+  age: 43
+}
