ca2edf70eab6fa387a56983a9beb905ccb9e49d1
ono-emi <ono-emi@gnavi.co.jp>
2023-06-16 13:40:52 +0900

    create src/anonymous-function.ts

---
+++ src/anonymous-function.ts
@@ -0,0 +1,13 @@
+export {};
+// アノテーション
+// let bmi: (height: number, weight: number) => number 
+
+let bmi: (height: number, weight: number) => number = function (
+  height: number,
+  weight:number 
+  ): number{
+    return weight/ ( height * height);
+  };
+
+
+console.log(bmi(1.78, 86));
